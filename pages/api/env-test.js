c71f84c054cc1b82ab8b396f216baa9029461cf6
tbbedacholli <thaman.bedacholli@example.com>
2025-07-20 02:27:30 -0700

    test env variables

--- pages/api/env-test.js
+++ pages/api/env-test.js
@@ -1,6 +1,7 @@
 // pages/api/env-test.js
 export default function handler(req, res) {
   res.status(200).json({
+    testVar: process.env.TEST_VAR || 'missing',
     region: process.env.MY_REGION ? 'set' : 'missing',
     bucket: process.env.MY_S3_BUCKET_NAME ? 'set' : 'missing',
     accessKey: process.env.MY_ACCESS_KEY_ID ? 'set' : 'missing',
