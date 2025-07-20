b79b646b4881c82acb35a4a0f810100e9fe3ae97
tbbedacholli <thaman.bedacholli@example.com>
2025-07-20 00:51:09 -0700

    updated env variables

--- pages/api/s3-data.js
+++ pages/api/s3-data.js
@@ -2,10 +2,10 @@
 import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
 
 const s3Client = new S3Client({
-  region: process.env.AWS_REGION || 'us-east-1',
+  region: process.env.MY_REGION || 'us-east-1',
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.MY_ACCESS_KEY_ID,
+    secretAccessKey: process.env.MY_SECRET_ACCESS_KEY,
   },
 });
 
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
 
   try {
     const command = new GetObjectCommand({
-      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Bucket: process.env.MY_S3_BUCKET_NAME,
       Key: key,
     });
 
